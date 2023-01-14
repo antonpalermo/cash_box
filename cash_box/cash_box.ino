77e688aebcc567beed5af133e151812fda6f3b2f
antonpalermo <ANPBusinessmail@gmail.com>
2023-01-14 15:49:58 +0800

    update color sensor

--- cash_box/cash_box.ino
+++ cash_box/cash_box.ino
@@ -3,12 +3,20 @@ void serial_init();
 // LED device state indicator.
 void device_state(unsigned long interval = 50);
 // collection procedures
-void execute_collection_procedures();
+void execute_collection_procedures(int red, int green, int blue);
 
+#include <tcs3200.h>
 #include <JC_Button.h>
 
+// button pins
 const int PIN_RESET = 4;
 const int PIN_COLLECT = 2;
+// color sensor pins
+const int PIN_S0 = 8;
+const int PIN_S1 = 9;
+const int PIN_S2 = 10;
+const int PIN_S3 = 11;
+const int PIN_OUTPUT = 12;
 
 // bool that checks of the LED indicator is on
 bool is_reset = false;
@@ -17,6 +25,8 @@ unsigned long current_millis = 0;
 
 Button reset_button(PIN_RESET);
 Button collect_button(PIN_COLLECT, 100);
+// color sensor setup.
+tcs3200 tcs(PIN_S0, PIN_S1, PIN_S2, PIN_S3, PIN_OUTPUT);  // (S0, S1, S2, S3, output pin)  //
 
 void setup() {
   serial_init();
@@ -30,6 +40,7 @@ void setup() {
   Serial.println("initializing serial complete...");
 }
 
+int red, green, blue;
 
 void loop() {
   // reset button (red) digital reading.
@@ -41,9 +52,16 @@ void loop() {
   // LED that indicates the device state.
   device_state();
 
+  // red color reading.
+  red = tcs.colorRead("r");
+  // green color reading.
+  green = tcs.colorRead("g");
+  // blue color reading.
+  blue = tcs.colorRead("b");
+
   // if collection button is pressed.
   if (collect_button.wasPressed()) {
     // execute collection procedures.
-    execute_collection_procedures();
+    execute_collection_procedures(red, green, blue);
   }
 }
--- cash_box/execute_collection_procedures.ino
+++ cash_box/execute_collection_procedures.ino
@@ -1,16 +1,23 @@
-/**
-This function should handle color sensor reading and storing of amount.
-*/
-void execute_collection_procedures() {
+
+void execute_collection_procedures(int red, int green, int blue) {
   Serial.println("[process]: executing collection procedures...");
   delay(1000);
-  Serial.println("[process]: looking for color patterns...");
-  delay(2000);
-  Serial.println("[info]: ammount pressent: ***This should be the ammount taken by the color sensor***");
-  delay(1000);
-  Serial.println("[info]: await collection approval...");
-  delay(1000);
-  Serial.println("[process]: approved storing ***amount captured*** ");
+  Serial.print("[process]: RGB value: ");
+
+  Serial.print(red);
+  Serial.print(", ");
+  Serial.print(green);
+  Serial.print(", ");
+  Serial.print(blue);
+
+  Serial.println("");
+
+  // delay(2000);
+  // Serial.println("[info]: ammount pressent: ***This should be the ammount taken by the color sensor***");
+  // delay(1000);
+  // Serial.println("[info]: await collection approval...");
+  // delay(1000);
+  // Serial.println("[process]: approved storing ***amount captured*** ");
   delay(2000);
   Serial.println("[info]: collection procedures complete...");
 }
